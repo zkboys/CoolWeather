bfd90f6a78edbf1a6e52048ce23a18b594f97cda
zkboys <876213774@qq.com>
2016-12-03 00:15:55 +0800

    android 数据库操作

--- app/src/main/java/com/zkboys/coolweather/activity/MainActivity.java
+++ app/src/main/java/com/zkboys/coolweather/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.zkboys.coolweather;
+package com.zkboys.coolweather.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.zkboys.coolweather.R;
 
 public class MainActivity extends AppCompatActivity {
 
